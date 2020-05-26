0305d8f1e044a35d96111664db42165d59f0468d
Santiago <smorenor@eafit.edu.co>
2020-05-26 12:40:37 -0500

    taller 4 parte 2

--- DjangoServer/measure/models.py
+++ DjangoServer/measure/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class Measure(models.Model):
-    id = models.UUIDField(primary_key=True, default=int, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     codigo = models.CharField(verbose_name='codigo', max_length=20,default='20181400')
     longitud = models.IntegerField(verbose_name='longitud',default=1)
     latitud = models.IntegerField(verbose_name='latitud',default=1)
--- DjangoServer/proyectointegrador1/settings.py
+++ DjangoServer/proyectointegrador1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o!llh+mgy*kul6b-mjn##idg+d@q)rri&&upxe753*4%wr8e25'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
